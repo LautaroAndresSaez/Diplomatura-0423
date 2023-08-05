9d69aebc4814cdd7b5ee091891d70bc85d6caee5
lautaro saez <lautarosaezdev@gmail.com>
2023-08-04 21:58:44 -0300

    x

--- unidad-2/clases/index.js
+++ unidad-2/clases/index.js
@@ -8,8 +8,8 @@ const b = 2;
 const suma = a + b;
 const resta = a - b;
 const multiplicacion = a * b;
-const division = a * b;
+const division = a / b;
 
-const modulo = a % a;
-const modulo2 = 140 % 7;
+const modulo = a % b;
+const modulo2 = 25 % 7;
 console.log(modulo2);
