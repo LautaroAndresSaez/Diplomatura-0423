3f838b939ff68b74a0479ec82012aabe55fe7b62
Lautaro saez <Lautarosaez@gmail.com>
2023-09-28 19:45:30 -0300

    feat: paso 1

---
+++ unidad-4/clases/clase-3/src/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const PORT = 3000;
+
+const tareas = [
+  {
+    nombre: "Comprar pan",
+    desc: "Ir a la panaderia",
+  },
+];
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hola");
+});
+
+app.listen(PORT, () => console.log(`Servidor corriendo en el puerto: ${PORT}`));
