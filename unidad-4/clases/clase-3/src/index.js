d4bad42dce60561b8bec96ab782bf550588d34c6
Lautaro saez <Lautarosaez@gmail.com>
2023-09-28 20:09:53 -0300

    feat: paso 2

--- unidad-4/clases/clase-3/src/index.js
+++ unidad-4/clases/clase-3/src/index.js
@@ -6,6 +6,10 @@ const tareas = [
     nombre: "Comprar pan",
     desc: "Ir a la panaderia",
   },
+  {
+    nombre: "Dar clases",
+    desc: "Dar clase 3",
+  },
 ];
 
 const app = express();
@@ -13,6 +17,16 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hola");
+});
+
+app.get("/tareas", (req, res) => {
+  res.send({ tareas });
+});
+
+app.post("/tareas", (req, res) => {
+  const tarea = req.body;
+  tareas.push(tarea);
+  res.send({ mensaje: "Tarea agregada!" });
 });
 
 app.listen(PORT, () => console.log(`Servidor corriendo en el puerto: ${PORT}`));
